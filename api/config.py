ab5288bfef1ceeb9b3c0f3f7bf6626e326eb14a5
hbaltz <hbaltz@gismartware.com>
2019-04-08 13:47:39 +0200

    :zap: Correct config for docker-compose

--- api/config.py
+++ api/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or f'postgresql+psycopg2://KPI:bO36k73G@localhost:5432/kpi_test' 
+        'DATABASE_URL') or f'postgresql+psycopg2://KPI:bO36k73G@db:5432/kpi_test' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
